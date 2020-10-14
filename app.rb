d84459dccc90c5d11cc5de2422a81ed211b0b578
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-14 02:21:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is "
+  end
+  
+  get '/hometown' do
+     "My hometown is "
+ end
+
+ get '/favorite-song' do
+   "My favorite song is "
+ end
+ 
 end
